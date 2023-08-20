22905232345b54cccf1a705b43aee28828ff27e3
Aastha Patel <114515584+Aastha-P@users.noreply.github.com>
2023-08-20 21:02:42 +0530

    Basics

--- Theory.py
+++ Theory.py
@@ -1,1 +1,15 @@
 #History of python
+#History and Basics of python
+#Python is--
+            #high level langage
+            #portable programming language
+            #object based
+            #Dyanamically typed language not statically type language
+            #Easy to learn
+            #highly readable language,the cost of program maintanance is reduced
+#It has--
+          #inbuilt automatic memory management,string,thread
+          #Easy debugging
+          
+#It supports modules and packages,it incourages program modularity and code reuse
+#In python variables are instance of classes
